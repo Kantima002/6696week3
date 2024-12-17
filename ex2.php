175f66e57e14a55fe159f1771d4f44bbf145ce2d
Kantima002 <kantima140747@gmail.com>
2024-12-17 10:57:30 +0700

    ex1

---
+++ ex2.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <!--เพิ่ม font -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Charmonman:wght@400;700&family=Itim&family=Krub:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;1,200;1,300;1,400;1,500;1,600;1,700&display=swap" rel="stylesheet">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    <style>
+       body{
+           font-family: "Krub", sans-serif;
+           font-weight: 400;
+           font-style: normal;
+        }
+
+</style>
+<!-- สิ้นสุดเพิ่ม font -->
+<meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+</head>
+<body>
+    <h1> โปรแกรม ภาษา HTML</h1>
+    664485002 นางสาวกานติมา เทียบคำ <br>
+    หมู่เรียน 66/96
+</body>
+</html>
